838d9812e62e94496aa334a4a08e4e442fc767fd
imaadityajha <144417366+imaadityajha@users.noreply.github.com>
2026-03-01 15:15:20 +0545

    Added port

--- server/src/app.js
+++ server/src/app.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 
 app.use(
     cors({
-        origin: ["http://localhost:5174", "http://localhost:5173", "http://localhost:3000"],
+        origin: ["http://localhost:5174", "http://localhost:5173", "http://localhost:3000" ,"https://ai-assistant-4-6s9y.onrender.com"],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
         credentials: true,
     })
